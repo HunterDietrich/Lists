9d62827d34ed7ebac552b5f05d2550653a212000
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-16 23:35:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#ARRAY
+grocery_list = ["apples", "bananas", "grapes", "twix"]
+print(grocery_list)
+
+#INDEXING
+print(grocery_list[2])
+print(grocery_list[0])
+
+#FOREACH LOOP
+for i in grocery_list:
+    print(i, "2.99")
+
+#FOR LOOP
+for i in range(len(grocery_list)):
+    print(grocery_list[i], "2.99")
